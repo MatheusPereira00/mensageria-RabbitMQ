3c11b98f2e6b465ad14d8c7ed69785800c0aca90
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-03-13 11:30:18 -0300

    first

---
+++ user/src/main/java/com/ms/user/models/UserModel.java
@@ -0,0 +1,24 @@
+package com.ms.user.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+}
